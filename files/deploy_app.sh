6cfdb46f662c507344589f8c14a41c1cf053a642
Bernd Prager <bernd@prager.ws>
2023-02-28 12:35:06 -0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  This is the ${PREFIX}'s app. Modfied as instructed in the workshop.
   <!-- END -->
 
   </div>
